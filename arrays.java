9886d5bbe4fc05157e69c776ac047b72f97e508c
jasmineC110181107 <93572304+jasmineC110181107@users.noreply.github.com>
2021-11-29 22:12:50 +0800

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+     public class arrays {
+         public static void main(String[] args) {
+             HashSet<String> number = new HashSet<String>();
+             number.add("0");
+             number.add("0");
+             number.add("1");
+             number.add("1");
+             number.add("1");
+             number.add("2");
+             number.add("2");
+             number.add("3");
+             number.add("3");
+             number.add("4");
+             System.out.println(number);
+         }
+     }
+
---
+++ prime.java
@@ -0,0 +1,18 @@
+public class prime {
+    public static void main(String[] args) {
+        int a = 0;
+        int k = 0;
+        for(int i = 2; i <= 100; i++){
+            for(int j = 2; j <= i; j++){
+                a = i % j;
+                if(a == 0){
+                    k = k + 1;
+                }
+            }
+        if(k == 1){
+                System.out.println(i);
+            }
+           k = 0;
+        }
+    }
+}

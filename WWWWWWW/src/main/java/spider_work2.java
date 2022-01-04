23505e914069e16f6ea4911a29d289b62ba5b999
C110181136 <0905817735a@gmail.com>
2022-01-04 13:09:57 +0800

    C110181136(1/4)

---
+++ WWWWWWW/src/main/java/spider_work2.java
@@ -0,0 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class spider_work2 {
+    public static void main(String[] args) {
+        try {
+            Document movie = Jsoup.connect("https://movies.yahoo.com.tw/showtimes").get();
+            System.out.println(movie.title());
+            for(int i = 1; i <= 10; i++){
+                String moviename = movie.select("body > div.showtime > div:nth-child(2) > ul > li:nth-child(" + i + ") > div > div > a:nth-child(1) > h2").text();
+                System.out.println("第" + i + "名:"+moviename);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("error" + e);
+        }
+
+    }
+}

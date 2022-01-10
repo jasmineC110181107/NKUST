201ded09191f6836d190a36bcfa8d0b56e6e0f80
C110181141 <93572225+C110181141@users.noreply.github.com>
2022-01-10 21:35:43 +0800

    Update javafinalexam.java
    TODAY IS MONDAY. UNHAPPY : (

--- final_eaxm/src/main/java/javafinalexam.java
+++ final_eaxm/src/main/java/javafinalexam.java
@@ -5,46 +5,39 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-public class spider_work2 {
+public class spider {
     public static void main(String[] args) throws IOException {
 
         try {
-            //for (int j = 0; j < 1; j++) {
-            //int page = j * 10;
-            Document doc = Jsoup.connect("https://movies.yahoo.com.tw/showtimes" ).get();
-            System.out.println(doc.title());
-            //System.out.println("==================================");
 
-            //int num = 0;
-            //if (num == 0) {
-            //for (int i = 1; i <= 10; i++) {
-            //String MovieName = doc.select("body > div.showtime > div:nth-child(2) > ul > li:nth-child(" + i + ") > div > div > a:nth-child(1) > h2").text();
-            //String EnglishName = doc.select("body > div.showtime > div:nth-child(2) > ul > li:nth-child(" + i + ") > div > div > a:nth-child(1) > h3").text();
-            //System.out.println("第" + i + "名 : " + MovieName + "\n" + "英文名稱 : " + EnglishName);
-            //System.out.println("==================================");
-            // }
-            //.....................................................................................................................................................
-            String URL_1 = "";
-            Element take = doc.select("div.main-distance li a").first();
-            URL_1 = take.attr("href");
-            Document index_1 = Jsoup.connect(URL_1).get();
-            System.out.println(URL_1);
-            System.out.println(index_1.title());
-            //System.out.println(take.attr("href"));
-            //String take1 = doc.select("main-distance").text();
-            //String take2 = doc.select("span").get(4).text();
-            //String take3 = doc.select("span").get(5).text();
-            //String take4 = doc.select("span").get(6).text();
-            //String take5 = doc.select("span").get(7).text();
-            ///String take6 = doc.select("span").get(8).text();
-            //System.out.println("類別 : " + take1 + "\n" + "上映日期 : " + take2 + "\n" + "片長 : " + take3 + "\n" + "發行公司 : " + take4 + "\n" + "IMDb分數 : " + take5 + "\n" + "導演 : " + take6 + "\n");
-            //把這坨丟進裡面標題可以顯示，剩下內容  :))
-//......................................................................................................................................................
+            Document doc = Jsoup.connect("https://movies.yahoo.com.tw/showtimes").get();
+            System.out.println("台北票房TOP10電影");
+            System.out.println("==================================");
+            //抓取電影的網址位置
+            Elements movieUrls = doc.select(".showtime .main-distance .ranking-list li");
+            for (Element Urls : movieUrls) {
+                //抓取各個電影網址
+                Element Url = Urls.select(".gabtn").first();
+                String movieUrl = Url.absUrl("href");
+                //讀取電影資料
+                Document movieDoc = Jsoup.connect(movieUrl).get();
+                Elements MovieInfos = movieDoc.select("#container #content_l .l_box_inner .table .movie_intro_info_r");
+                //讀取並輸出所有資料
+                for (Element MovieInfo : MovieInfos){
+                    String Chinese = MovieInfo.select("h1").text();
+                    String English = MovieInfo.select("h3").text();
+                    System.out.printf("電影中文名稱：%s\n電影英文名稱：%s\n" ,Chinese,English);
+                }
+               // String URL_1 = "";
+               // Element take = doc.select("div.main-distance li a").first();
+               // URL_1 = take.attr("href");
+               // Document index_1 = Jsoup.connect(URL_1).get();
+               // System.out.println(URL_1);
+               // System.out.println(index_1.title());
 
-            //String take = doc.select("showtime > main-distance > ranking-list > li > href").text();
-            //System.out.print(take);   // .......這個搞不定
-        } catch (Exception e) {
-            System.out.println("error" + e);
+                }
+            } catch(Exception e){
+                System.out.println("error" + e);
+            }
         }
-    }
 }
